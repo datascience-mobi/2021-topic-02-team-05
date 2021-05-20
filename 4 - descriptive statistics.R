a85dfa940b62216b5bb9e98ac973603e40cdd7cd
LineWe <82637313+LineWe@users.noreply.github.com>
2021-05-20 11:44:54 +0200

    Update 4 - descriptive statistics.R

--- 4 - descriptive statistics.R
+++ 4 - descriptive statistics.R
@@ -1,7 +1,9 @@
 #heatmap of all our genes of interest on all 14 chips
 #with the pheatmap function
 library(pheatmap)
+setwd("/Users/Line/Documents/tra_project/thyroid_cancer_sessions/plots")
 pheatmap(thyroid.expr.matrix.sub, 
+         main = "gene expression of thyroid TRA's \nin PTC and healthy thyroid tissue (GSE35570) \n(Handkiewicz-Junak et all., 2016)",
          cluster_rows = F, 
          cluster_cols = F, 
          cellheight = 3, 
