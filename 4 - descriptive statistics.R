80dce717a966c662f336827613a2b3ee4473264a
LineWe <82637313+LineWe@users.noreply.github.com>
2021-07-07 10:51:04 +0200

    Update 4 - descriptive statistics.R

--- 4 - descriptive statistics.R
+++ 4 - descriptive statistics.R
@@ -17,14 +17,14 @@ pheatmap(thyroid.expr.matrix.sub,
 
 
 #distribution our genes over chromosomes (out of human TRA lists=> List e and f can be ignored!)
-         #preparation=> I need a matrix with the information "chromosome location" for each gene
+#preparation => I need a matrix with the information "chromosome location" for each gene
 tiss1=unique(as.character(b[,11])) # "thyroid"
 th.genes1= b [b$Max_tissue %in% tiss1[31],]
 dim(th.genes1)
 
-tissues2= unique(as.character(c[,11])) 
-length(tissues2) 
-th.genes2= c[c$max.tissue %in% tissues2[35],  ]
+tiss2= unique(as.character(c[,11])) 
+length(tiss2) 
+th.genes2= c[c$max.tissue %in% tiss2[35],  ]
 dim(th.genes2)
 
 tiss3=unique(as.character(d[,11])) #"thyroid_gland"
@@ -47,7 +47,7 @@ colnames(genes2)= c("gene.symbol", "chromosome")
 colnames(genes3)= c("gene.symbol", "chromosome")
 colnames(genes6)= c("gene.symbol", "chromosome")
 
-allgenesandchromosomes= bind_rows(genes1,genes2,genes3,genes6) # all genes of all TRA lists together
+allgenesandchromosomes= rbind(genes1,genes2,genes3,genes6) # all genes of all TRA lists together
 uniquegenes= duplicated(allgenesandchromosomes$gene.symbol) # selecting unique genes only
 Index= which(uniquegenes==FALSE )
 uniqueinfos=allgenesandchromosomes[Index,]
@@ -56,27 +56,25 @@ indexx=which(uniqueinfos$gene.symbol %in% symbol) # which genes are examined on 
 finalinfos= uniqueinfos[indexx,]
 Ychrom= data.frame("inventedGene", "Y")
 colnames(Ychrom)= c("gene.symbol", "chromosome")
-finalinfos= bind_rows(finalinfos, Ychrom)
+finalinfos= rbind(finalinfos, Ychrom)
 View(finalinfos) 
 
 #final plots
-chrnumbers= table(finalinfos[,2])
-chromosomes= chrnumbers[c(2,13,17:23,3:12,14:16,25)]
-chromswith0= c(chromosomes,chrnumbers[24])
-chromswithY= c(chromosomes, chrnumbers[26])
+chrnumbers= table(finalinfos[,2]) #wie ift ein chromosme vorkommt 
+chromosomes= chrnumbers[c(2,13,17:23,3:12,14:16,26)]
+chromswith0= c(chromosomes,chrnumbers[25])
+chromswithY= c(chromosomes, chrnumbers[27])
 chromswithY[24]= 0
+View(chromosomes)
 
 library(viridisLite)
 par(mai= c(2,2,2,2))
+pdf(file="piechart_chromosomes.pdf")
 pie(chromswith0, clockwise= TRUE,border= FALSE, col= c(plasma(23),"lightgoldenrodyellow"), init.angle=180, radius=1, main=" Distribution of \n thyroid specific genes \n over chromosomes \n Dinkelacker, 2019, PhD thesis, University of Heidelberg")
+dev.off()
+pdf(file="barplot_chromosomes.pdf")
 par(las=1)
-barplot(chromswithY, col=plasma(23), main=" Distribution of thyroid specific genes over chromosomes \n Dinkelacker, 2019, PhD thesis, University of Heidelberg", xlab= "chromosomes", ylab= "number of genes", ylim=c(0,20))
+barplot(chromswithY, col=plasma(23), cex.names=0.5,main=" Distribution of thyroid specific genes over chromosomes \n Dinkelacker, 2019, PhD thesis, University of Heidelberg", xlab= "chromosomes", ylab= "number of genes", ylim=c(0,20))
 abline(h=c(5,10,15,20), lty=3) # makes the grey lines. Tiping the barplot-code again, but with "add=TRUE" puts the lines in the background
-barplot(chromswithY, col=plasma(23), add=TRUE, main=" Distribution of thyroid specific genes over chromosomes \n Dinkelacker, 2019, PhD thesis, University of Heidelberg", xlab= "chromosomes", ylab= "number of genes", ylim=c(0,20))
-
-
-
-
-
-
-
+barplot(chromswithY, col=plasma(23), add=TRUE, cex.names=0.5,main=" Distribution of thyroid specific genes over chromosomes \n Dinkelacker, 2019, PhD thesis, University of Heidelberg", xlab= "chromosomes", ylab= "number of genes", ylim=c(0,20))
+dev.off()
